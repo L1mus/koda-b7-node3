edab1d49ac7c4163b2e34e81dccf295aa38cb0ac
l1mus <user01@DESKTOP-G0IBLL4.localdomain>
2026-03-04 09:19:57 +0700

    add plugins and ignores

--- minitask-3/eslint.config.js
+++ minitask-3/eslint.config.js
@@ -5,6 +5,12 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
+  { ignores: ["node_modules"] },
+  {
+    plugins: {
+      stylistic: stylistic,
+    },
+  },
   {
     rules: {
       "no-unused-vars": "warn",
